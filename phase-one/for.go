0c649cf989c8974903347873e89bb8cf5bab750d
libai <18101611430@163.com>
2026-07-19 20:10:43 +0800

    增加了for.go

---
+++ phase-one/for.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sum int
+	for a, b := 9, 30; a >= 0 && b > a; a, b = a-1, b/2 {
+		sum += a
+		if a == 4 {
+			break
+		}
+		if a == 5 {
+			continue
+		}
+	}
+	fmt.Println(sum)
+}
--- phase-one/if.go
+++ phase-one/if.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func main8() {
 	if 5 > 9 {
 		fmt.Println("AAA")
 	}
