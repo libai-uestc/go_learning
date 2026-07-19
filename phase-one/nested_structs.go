7dfa013a06a8a879b1437592761ae1b1716f7603
libai <18101611430@163.com>
2026-07-19 16:52:06 +0800

    嵌套结构体

---
+++ phase-one/nested_structs.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type Residence struct {
+	Province string
+	City     string
+}
+
+type User struct {
+	Id            int
+	Score         float64
+	Name, address string
+	residence     Residence
+	info          struct { //匿名结构体
+		Age   int
+		mouth int
+		year  int
+	}
+	Father *User // 指针的初始化就是nil
+}
+
+type TreeNode struct {
+	Data       int
+	LeftChild  *TreeNode
+	RightChild *TreeNode
+}
+
+func (user User) hello() {
+	fmt.Println("My Name is", user.Name)
+}
+
+func main() {
+	var user User
+	user.Score = 100
+	user = User{Id: 32, address: "China", Name: "libai"}
+	fmt.Println(user.Name)
+	u := User{12, 100, "gogogo", "China_sh", Residence{"bj", "yz"},
+		struct {
+			Age   int
+			mouth int
+			year  int
+		}{18, 12, 2026},
+		&User{},
+	}
+	fmt.Printf("%v\n", u)
+	fmt.Printf("%+v\n", u)
+	fmt.Printf("%#v\n", u)
+	u.hello()
+
+	ue := u
+	ue.Name = "lb"
+	_ = ue
+}
--- phase-one/struct.go
+++ phase-one/struct.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // 结构体：把多个变量进行打包构成了一个整体
-type User struct {
+type User1 struct {
 	// 成员变量
 	Id            int
 	Score         float64
@@ -11,16 +11,16 @@ type User struct {
 }
 
 // 成员方法
-func (user User) hello() {
+func (user User1) hello1() {
 	fmt.Println("My Name is", user.Name)
 }
 
 func main5() {
-	var u User
-	u = User{Id: 10, Score: 100, Name: "libai", address: "China"} //Score可以不赋值，默认是0值，整数或浮点数不赋值默认是0，布尔默认是false，字符串默认是空字符串
-	fmt.Println(u.Name)
-	u = User{99, 100, "libai", "China"}
-	u.hello()
+	var uu User1
+	uu = User1{Id: 10, Score: 100, Name: "libai", address: "China"} //Score可以不赋值，默认是0值，整数或浮点数不赋值默认是0，布尔默认是false，字符串默认是空字符串
+	fmt.Println(uu.Name)
+	uu = User1{99, 100, "libai", "China"}
+	uu.hello1()
 
 	// 匿名结构体 只在一个场景，不需要重新使用
 	type student struct {
@@ -35,7 +35,7 @@ func main5() {
 
 	student_1.Name = "libai"
 	student_1.Age = 18
-	u.Name = student_1.Name
+	uu.Name = student_1.Name
 	a := student_1.Age
 	_ = a
 
