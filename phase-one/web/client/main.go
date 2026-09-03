7e8eab9cf1e252fe754d51ac90e4d1eac0018de9
libai <18101611430@163.com>
2026-09-03 20:18:38 +0800

    新增http编程的client-main.go,server-main.go

---
+++ phase-one/web/client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func HttpObservation() {
+	fmt.Println(strings.Repeat("*", 30) + "GET" + strings.Repeat("*", 30))
+	resp, err := http.Get("http://127.0.0.1:5678/obs?name=libai&age=18")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	fmt.Printf("response proto: %s\n", resp.Proto) // http1.1
+	if major, minor, ok := http.ParseHTTPVersion(resp.Proto); ok {
+		fmt.Printf("http major version %d, http minor version %d\n", major, minor)
+	}
+	fmt.Printf("response status: %s\n", resp.Status)          // 200 OK
+	fmt.Printf("response status code: %d\n", resp.StatusCode) // 200
+
+	for key, values := range resp.Header {
+		fmt.Printf("%s:%v\n", key, values)
+		if key == "Date" {
+			if tm, err := http.ParseTime(values[0]); err == nil {
+				fmt.Printf("server time %s\n", tm.Format("2006-01-02 15:04:05"))
+			}
+		}
+	}
+
+	fmt.Println("response body:")
+	io.Copy(os.Stdout, resp.Body) // 两个io数据流的拷贝
+	os.Stdout.WriteString("\n\n")
+
+}
+
+func main() {
+	HttpObservation()
+}
---
+++ phase-one/web/server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	// "os"
+)
+
+func HttpObservation(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("request method: %s\n", r.Method)
+	fmt.Printf("request host: %s\n", r.Host) // 服务端host
+	fmt.Printf("request url: %s\n", r.URL)
+	fmt.Printf("request proto: %s\n", r.Proto)
+	fmt.Println("request header")
+	for key, values := range r.Header { // 变量类型是map,map的key是字符串，map的value是字符串切片
+		fmt.Printf("%s: %v\n", key, values)
+	}
+	fmt.Println()
+	fmt.Printf("request body: ")
+	// io.Copy(os.Stdout,r.Body) // 把r.Body流里的内容拷贝到os.Stdout流里
+	if body, err := io.ReadAll(r.Body); err == nil {
+		fmt.Println(string(body))
+	}
+	fmt.Println()
+
+	w.Header().Add("tRAce-id", "4723956498105") // Trace-Id
+	w.WriteHeader(http.StatusBadRequest)        // 如果这一行被注释掉，则会默认把状态码设置为http.StatusOK
+	w.Write([]byte("Hello Boy\n"))
+	w.Write([]byte("Hello Girl\n"))
+	fmt.Fprint(w, "Hello Boy\n") // 和w.Write([]byte("Hello Boy\n"))的效果是一样的
+	fmt.Println(strings.Repeat("*", 60))
+}
+
+func main() {
+	// 定义路由
+	http.HandleFunc("/obs", HttpObservation)
+
+	// 启动Http Server
+	if err := http.ListenAndServe("127.0.0.1:5678", nil); err != nil {
+		panic(err)
+	}
+}
